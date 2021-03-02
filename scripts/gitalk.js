d989bdc22261a6c9e6da9c2d0fac10f6f6cea0cb
Yibai Zhang <xm1994@gmail.com>
2021-03-03 01:53:30 +0800

    feat: Add gitalk comment.
    Signed-off-by: Yibai Zhang <xm1994@gmail.com>

---
+++ scripts/gitalk.js
@@ -0,0 +1,17 @@
+hexo.extend.filter.register('after_post_render', function(data) {
+    var gitalkConfig = hexo.config.gitalk;
+    if (gitalkConfig) {
+        data.content = data.content + '\n<div id="gitalk-container"></div>\n<script src="//cdn.jsdelivr.net/npm/blueimp-md5@2.18.0/js/md5.min.js"></script><link rel="stylesheet" href="//cdn.jsdelivr.net/npm/gitalk/dist/gitalk.css"><script src="//cdn.jsdelivr.net/npm/gitalk/dist/gitalk.min.js"></script>';
+        var str = `
+        <script>
+        var gitalkConfig = ${JSON.stringify(gitalkConfig)};
+        gitalkConfig.id = md5(location.pathname);
+        var gitalk = new Gitalk(gitalkConfig);
+        gitalk.render("gitalk-container");
+        </script>`;
+
+        data.content = data.content + '\n' + str;
+    }
+
+    return data;
+});
