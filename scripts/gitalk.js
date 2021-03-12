bfaa9f91f012910f5d9cf614217f3669c069045c
Yibai Zhang <xm1994@gmail.com>
2021-03-12 17:31:41 +0800

    feat(gitalk): Use title as gitalk id.

--- scripts/gitalk.js
+++ scripts/gitalk.js
@@ -4,8 +4,9 @@ hexo.extend.filter.register('after_post_render', function(data) {
         data.content = data.content + '\n<div id="gitalk-container"></div>\n<script src="//cdn.jsdelivr.net/npm/blueimp-md5@2.18.0/js/md5.min.js"></script><link rel="stylesheet" href="//cdn.jsdelivr.net/npm/gitalk/dist/gitalk.css"><script src="//cdn.jsdelivr.net/npm/gitalk/dist/gitalk.min.js"></script>';
         var str = `
         <script>
+        var post_title = ${JSON.stringify(data.title)};
         var gitalkConfig = ${JSON.stringify(gitalkConfig)};
-        gitalkConfig.id = md5(location.pathname);
+        gitalkConfig.id = md5(post_title);
         var gitalk = new Gitalk(gitalkConfig);
         gitalk.render("gitalk-container");
         </script>`;
